9703783e625519c1379a2f73b4fc606e814c1abc
root <!gown119>
2019-10-24 15:21:05 -0400

    Commit Inicial

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dialect:'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'root',
+    database: 'gobarber',
+    define:{
+        timestamps:true,
+        underscored:true,
+        underscodedAll:true,
+
+
+    },
+
+};
